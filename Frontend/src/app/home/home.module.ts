3ece10c7d4d393c6bd8ba4b2bf96575fb394029f
Saya k <saya-k-care@hotmail.com>
2025-05-27 15:32:02 +0800

    updates

--- Frontend/src/app/home/home.module.ts
+++ Frontend/src/app/home/home.module.ts
@@ -1,11 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HeaderModule } from '../shared-modules/header/header.module';
 import { FooterModule } from '../shared-modules/footer/footer.module';
 
