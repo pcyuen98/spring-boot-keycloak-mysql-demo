671504508635f23c3fc12c71f8f4bb4f1abbf098
Saya k <saya-k-care@hotmail.com>
2025-05-27 15:32:10 +0800

    updates

--- Backend/src/main/java/com/demo/keycloak/HTTPKeycloakApplication.java
+++ Backend/src/main/java/com/demo/keycloak/HTTPKeycloakApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HTTPSessionApplication {
+public class HTTPKeycloakApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HTTPSessionApplication.class, args);
+        SpringApplication.run(HTTPKeycloakApplication.class, args);
     }
 
 }
